839e1b3a8ffb1895f9d42acb3cb2b528ada310b3
juandv30 <78824982+juandv30@users.noreply.github.com>
2021-08-25 23:07:50 -0500

    Add vehicle type

---
+++ Vehicless.API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicless.API.Data
+{
+    public class DataContext: DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<VehicleType> VehiclesTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
---
+++ Vehicless.API/Migrations/20210826035012_AddTableVehicleType.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicless.API.Migrations
+{
+    public partial class AddTableVehicleType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VehiclesTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiclesTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehiclesTypes_Description",
+                table: "VehiclesTypes",
+                column: "Description",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehiclesTypes");
+        }
+    }
+}
